bf159a998818d99474a4fbb0ca0f99fc39e2aa92
Quentin <quentin.flueckiger@hispeed.ch>
2019-05-13 14:40:07 +0200

    Finished connect4
    - Finished connect4
    - Some unused code which were originaly there to allow player to change board size and number of piece in a row (have to tweek the condition/if statement to make it work)
    - Might still have to implement alpha-beta pruning duno

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Connect4
 
 
-Basic game of Connect 4.
+Basic game of Connect 4 programmed with Basili Michel for the modul Game theory.
--- connect4.py
+++ connect4.py
@@ -2,7 +2,9 @@ import numpy as np
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
+PIECES_TO_WIN = 4
 
+#Method definitions part
 #Initialize board as numpy array
 def create_board():
     board = np.zeros((ROW_COUNT,COLUMN_COUNT))
@@ -26,6 +28,61 @@ def get_next_open_row(board, column):
 def print_board(board):
     print(np.flip(board,0))
 
+#Brute force check for winning moves (maybe to change to something more optimal)
+#And add possibility to change board size, number of same pieces needed to win
+def winning_move(board, piece):
+    #Check horizontal
+    for c in range(COLUMN_COUNT-(PIECES_TO_WIN-1)):
+        winning = False
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+               return True
+            if board[r][c] == piece :
+                for p in range(1,PIECES_TO_WIN):
+                    if board[r][c+p] == piece:
+                        winning = True
+                    else:
+                        winning = False
+                #if winning :
+                #    return True
+
+    #Check vertical
+    for c in range(COLUMN_COUNT):
+        winning = False
+        for r in range(ROW_COUNT-(PIECES_TO_WIN-1)):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+               return True
+            if board[r][c] == piece:
+                for p in range(1,PIECES_TO_WIN):
+                    if board[r+p][c] == piece:
+                        winning = True
+                    else:
+                        winning = False
+                #if winning :
+                #    return True
+
+    #Check positive diaganols
+    for c in range(COLUMN_COUNT-(PIECES_TO_WIN-1)):
+        winning = False
+        for r in range(ROW_COUNT-(PIECES_TO_WIN-1)):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+               return True
+            if board[r][c] == piece :
+                for p in range (1,PIECES_TO_WIN):
+                    if board[r+p][c+p] == piece:
+                            winning = True
+                    else:
+                        winning = False
+                #if winning :
+                #    return True
+
+    #Check negative diaganols
+    for c in range(COLUMN_COUNT-(PIECES_TO_WIN-1)):
+        for r in range(PIECES_TO_WIN-1, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+               return True
+
+#Core program
 board = create_board()
 game_over = False
 turn = 0
@@ -40,6 +97,10 @@ while not game_over:
             row = get_next_open_row(board,column)
             drop_piece(board,row,column,1)
 
+            if winning_move(board,1) :
+                print("Player 1 wins.")
+                game_over = True
+
     #Ask player 2 for input
     else:
         column = int(input("Player 2 make your selection (0-6):"))
@@ -47,6 +108,10 @@ while not game_over:
         if is_valid_location(board, column) :
             row = get_next_open_row(board,column)
             drop_piece(board,row,column,2)
+
+            if winning_move(board,2) :
+                print("Player 2 wins.")
+                game_over = True
 
     print_board(board)
 
